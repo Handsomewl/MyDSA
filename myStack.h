702d1e05daaa72470eb1fa76473c1f0faa3c89f0
Handsomewl <wl.tsinghua.ee@gmail.com>
2021-07-14 01:36:58 +0800

    add convert algorithm

--- myStack.h
+++ myStack.h
@@ -4,7 +4,7 @@
 using namespace std;
 
 // 实现一个手写栈，并制定相关的程序测试;
-template<typename T> class Stack{		// public vector<T> 公有继承自向量类；
+template<typename T> class Stack{		// public vector<T> 公有继承自向量类；添加后会导致一些数据和接口泄露，不推荐
 private:
 	vector<T> data;
 public:
@@ -43,5 +43,18 @@ template<typename T>void Stack<T>:: output() {	// display and pop all elements;
 		cout << this->top();					// display the top element;
 		this->Pop();							// Pop the top element;
 	}
+	cout << endl;
 }
 
+// implement some algorithms
+
+// 进制转换算法，参考邓俊辉老师DSA MOOC
+void convert(Stack<char>& s, _int64 n, int base) {	// n对应输入数，base表示进制
+	static char digit[] = {
+		'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'
+	};
+	while (n > 0) {
+		s.Push(digit[n % base]);	// 余数入栈
+		n /= base;					// 更新商
+	}
+}
--- test.cpp
+++ test.cpp
@@ -3,12 +3,17 @@
 using namespace std;
 
 int main() {
+	// 测试基本功能
 	Stack <int> st;
 	const int a[5] = { 1,2,3,4,5 };
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 5; i++)
 		st.Push(a[i]);
-	}
 	st.output();
+
+	// 测试进制转换
+	Stack <char> s;
+	convert(s, 2013,5);
+	s.output(); 
 
 	return 0;
 }
